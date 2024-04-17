8514ce477698783c0efba10ebd555b0344d0c316
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-17 16:02:41 +0200

    feat(signals): :sparkles: add singnals and computedSignals in counterPage

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter-page',
   templateUrl: './counter-page.component.html',
-  styleUrl: './counter-page.component.css'
+  styleUrl: './counter-page.component.css',
 })
 export class CounterPageComponent {
-
+  counter = signal(10);
+  squaredCounter = computed(() => this.counter() * this.counter());
+  increaseBy(value: number) {
+    this.counter.update((current) => current + value);
+  }
 }
