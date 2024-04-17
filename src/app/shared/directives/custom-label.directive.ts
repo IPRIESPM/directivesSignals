987e214967a41fa5784118f05e3f498b3240d9e9
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-17 13:35:12 +0200

    feat(directive): :sparkles: add show error messages

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -6,6 +6,7 @@ import {
   inject,
   input,
 } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]',
@@ -15,11 +16,18 @@ export class CustomLabelDirective implements OnInit {
 
   private htmlElement?: HTMLElement;
   private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
 
   @Input()
   set color(value: string) {
     this._color = value;
     this.setStyle();
+  }
+
+  @Input()
+  set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessages();
   }
 
   constructor(private elementRef: ElementRef<HTMLElement>) {
@@ -33,5 +41,32 @@ export class CustomLabelDirective implements OnInit {
   setStyle(): void {
     if (!this.htmlElement) return;
     this.htmlElement.style.color = this._color;
+  }
+
+  setErrorMessages(): void {
+    if (!this.htmlElement) return;
+    if (!this._errors) {
+      this.htmlElement.textContent = 'No hay errores';
+      return;
+    }
+    const errors = Object.keys(this._errors);
+    this.htmlElement.textContent = errors.join(', ');
+
+    if (errors.includes('required')) {
+      this.htmlElement.textContent = 'Campo requerido';
+      return;
+    }
+
+    if (errors.includes('minlength')) {
+      const min = this._errors!['minlength']['requiredLength'];
+      const current = this._errors!['minlength']['actualLength'];
+      const message = `El mínimo de caracteres es ${min}, actualmente tienes ${current}`;
+      this.htmlElement.textContent = message;
+      return;
+    }
+
+    if (errors.includes('email')) {
+      this.htmlElement.textContent = 'No tiene formato de email';
+    }
   }
 }
