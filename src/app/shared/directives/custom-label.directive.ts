3afdb5e53883ce010d3a8d7c4291aa2014cc5d28
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-17 13:19:07 +0200

    feat(directive): :sparkles: add custom color

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,11 @@
-import { Directive, ElementRef, OnInit, inject } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnInit,
+  inject,
+  input,
+} from '@angular/core';
 
 @Directive({
   selector: '[customLabel]',
@@ -7,13 +14,24 @@ export class CustomLabelDirective implements OnInit {
   // private elementRef: ElementRef<HTMLElement> = inject(ElementRef);
 
   private htmlElement?: HTMLElement;
+  private _color: string = 'red';
+
+  @Input()
+  set color(value: string) {
+    this._color = value;
+    this.setStyle();
+  }
+
   constructor(private elementRef: ElementRef<HTMLElement>) {
     this.htmlElement = elementRef.nativeElement;
-    console.log(this.htmlElement);
-
     this.htmlElement.textContent = 'Custom Label';
   }
   ngOnInit(): void {
-    console.log('CustomLabelDirective ngOnInit');
+    this.setStyle();
+  }
+
+  setStyle(): void {
+    if (!this.htmlElement) return;
+    this.htmlElement.style.color = this._color;
   }
 }
