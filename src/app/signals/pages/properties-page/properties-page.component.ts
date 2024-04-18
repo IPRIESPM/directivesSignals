bd967096c673157443b16ae3bdf80a60759faa90
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-18 10:59:43 +0200

    feat(signals): :sparkles: add mutations on signals

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-properties-page',
   templateUrl: './properties-page.component.html',
-  styleUrl: './properties-page.component.css'
+  styleUrl: './properties-page.component.css',
 })
 export class PropertiesPageComponent {
+  user = signal<User>({
+    id: 2,
+    email: 'janet.weaver@reqres.in',
+    first_name: 'Janet',
+    last_name: 'Weaver',
+    avatar: 'https://reqres.in/img/faces/2-image.jpg',
+  });
 
+  fullName = computed(
+    () => `${this.user().first_name} ${this.user().last_name}`
+  );
+
+  onFieldUpdated(field: keyof User, value: string) {
+    this.user.update((current) => {
+      switch (field) {
+        case 'email':
+          current.email = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'avatar':
+          current.avatar = value;
+          break;
+        case 'id':
+          current.id = Number(value);
+      }
+      return current;
+    });
+  }
 }
